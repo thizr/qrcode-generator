1326baef880f2743b874fe87a06142b4e87c1f8f
thzr <thiernondiaye122@gmail.com>
2022-01-04 18:14:40 +0000

    generate qrcode text

--- controllers/qrcode.controller.js
+++ controllers/qrcode.controller.js
@@ -2,13 +2,18 @@
 const qrcode = require('qrcode');
 
 exports.generate = (req, res) => {
-    console.log(req.params.text);
-    qrcode.toDataURL(req.params.text, (err, src) => {
-        if (err) res.send("Error occured");
-      
-        // Let us return the QR code image as our response and set it to be the source used in the webpage
-        res.render("index", { src });
-        // res.status(200).json({qrcode : src})
-        console.log(src);
-    });
+    qrcode.toDataURL(req.body.data)
+    .then((qrcode) => {
+        res.render('index', {
+            succes : true,
+            message : "QR Code Generated",
+            data : qrcode});
+    })
+    .catch((error) => {
+        res.status(500).json({
+            succes : false,
+            message : "Une erreur est survenue",
+            data : error});
+    })
+   
 }
--- routes/qrcode.route.js
+++ routes/qrcode.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const qrcodeCtrl = require('../controllers/qrcode.controller');
 
-router.get('/generate/:text', qrcodeCtrl.generate);
+// router.get('/generate/:data', qrcodeCtrl.generate);
+router.post('/generate', qrcodeCtrl.generate);
 
 module.exports = router
